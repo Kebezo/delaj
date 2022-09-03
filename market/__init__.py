b31bb8e71245dc01501d2cd7e2f5bb7f775b04bb
Kebezo <112317873+Kebezo@users.noreply.github.com>
2022-09-03 21:10:35 +0200

    'popravi'

--- market/__init__.py
+++ market/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qszufkbkhbrbfr:809d522b23bad496eccc9e86d31c7e251308eb32663cc3bf9c7b4e564ae0bedd@ec2-34-199-68-114.compute-1.amazonaws.com:5432/d5bb8o015bpf4i'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://qszufkbkhbrbfr:809d522b23bad496eccc9e86d31c7e251308eb32663cc3bf9c7b4e564ae0bedd@ec2-34-199-68-114.compute-1.amazonaws.com:5432/d5bb8o015bpf4i'
 #randomly genereirano hexadecimal stevilo
 app.config['SECRET_KEY'] = '52627e432fe1def0e3834e53'
 bcrypt = Bcrypt(app)
